4656e5dd0e7125386592750e2dad089afdce179d
unknown <bradleyaw@gmail.com>
2020-02-26 08:05:52 -0500

    updates

---
+++ mPlayers.js
@@ -0,0 +1,18 @@
+export default [
+    {id: 1, name: "Garett Bergquist", wins: 0, losses: 0, points: 0},
+    {id: 2, name: "Lane Blundell", wins: 0, losses: 0, points: 0},
+    {id: 3, name: "Alex Bradley", wins: 0, losses: 0, points: 0},
+    {id: 4, name: "Matt Carucci", wins: 0, losses: 0, points: 0},
+    {id: 5, name: "Jake Clopton", wins: 0, losses: 0, points: 0},
+    {id: 6, name: "Steve Cozzolongo", wins: 0, losses: 0, points: 0},
+    {id: 7, name: "Ben Djernes", wins: 0, losses: 0, points: 0},
+    {id: 8, name: "Josh Duc", wins: 0, losses: 0, points: 0},
+    {id: 9, name: "Aaron Von Der Embse", wins: 0, losses: 0, points: 0},
+    {id: 10, name: "Vasyl Martniuk", wins: 0, losses: 0, points: 0},
+    {id: 11, name: "Adam Meier", wins: 0, losses: 0, points: 0},
+    {id: 12, name: "Rob Opalka", wins: 0, losses: 0, points: 0},
+    {id: 13, name: "Scotty Poythress", wins: 0, losses: 0, points: 0},
+    {id: 14, name: "John Sutton", wins: 0, losses: 0, points: 0},
+    {id: 15, name: "Nate Vaughn", wins: 0, losses: 0, points: 0}
+    ]
+

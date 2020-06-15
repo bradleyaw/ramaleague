b0c78f004be04cfa9f29b928c86c71dbbd9da0bd
unknown <bradleyaw@gmail.com>
2020-06-14 21:11:45 -0400

    week 2 changes

--- mens/js/mens.js
+++ mens/js/mens.js
@@ -1,8 +1,6 @@
 import players from './players.js';
 import weeklyResults from './weekly.js'
-console.log('hey')
-console.log(weeklyResults)
-console.log(players)
+
 const rankPlayers = (playerArray) => {
     return playerArray.sort(function (a, b) { return b.wins/b.losses - a.wins/a.losses || b.points - a.points })
 }
@@ -23,9 +21,9 @@ const playersRanked = rankPlayers(players);
 const pItems = playersRanked.map((player, index) => `<tr>
                                         <th scope="row">${++index}</th>
                                         <td>${player.name}</td>
-                                        <td>${player.wins}</td>
-                                        <td>${player.losses}</td>
-                                        <td>${typeof(player.wins/player.losses) === 'integer' ? player.wins/player.losses : player.wins}</td>
+                                        <td>${Math.round(player.wins)}</td>
+                                        <td>${Math.round(player.losses)}</td>
+                                        <td>${typeof(player.wins/player.losses) === 'integer' ? Math.round(player.wins/player.losses) : Math.round(player.wins)}</td>
                                         <td>${Math.round(player.points)}</td>                
                                     </tr>`)
 

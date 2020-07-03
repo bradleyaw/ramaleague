47e840c161260c9fdde68b03eaa9187cd111002b
unknown <bradleyaw@gmail.com>
2020-07-03 17:12:00 -0400

    Update coed teams and weekly mens

--- mens/js/fixtures/weekly.js
+++ mens/js/fixtures/weekly.js
@@ -61,7 +61,7 @@ export default [
     },
     {
         week: 4,
-        byes: [12, 13],
+        byes: [11, 12],
         results: [
             { id: 0, name: "Garett Bergquist", wins: 1, losses: 2, points: -8 },
             { id: 1, name: "Lane Blundell", wins: 1, losses: 2, points: 0 },
@@ -81,22 +81,40 @@ export default [
     },
     {
         week: 5,
-        byes: [0, 12],
-        results: [],
+        // Matt requested
+        byes: [3, 12],
+        results: [
+            { id: 0, name: "Garett Bergquist", wins: 2, losses: 1, points: 13 },
+            { id: 1, name: "Lane Blundell", wins: 1, losses: 2, points: 1 },
+            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: 7 },
+            { id: 3, name: "Matt Carucci", wins: 0, losses: 0, points: 0 },
+            { id: 4, name: "Jake Clopton", wins: 1, losses: 2, points: -5 },
+            { id: 5, name: "Steve Cozzolongo", wins: 2, losses: 1, points: 7 },
+            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: -3 },
+            { id: 7, name: "Josh Duc", wins: 2, losses: 1, points: -1 },
+            { id: 8, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 5 },
+            { id: 9, name: "Vasyl Martniuk", wins: 3, losses: 0, points: 9 },
+            { id: 10, name: "Adam Meier", wins: 0, losses: 3, points: -17 },
+            { id: 11, name: "Rob Opalka", wins: 1, losses: 2, points: -5 },
+            { id: 12, name: "Scott Poythress", wins: 0.1, losses: 0.1, points: 0 },
+            { id: 13, name: "Peter Chavez", wins: 0, losses: 3, points: -11 }
+        ],
     },
     {
         week: 6,
-        byes: [1, 12],
+        // Jake requested
+        byes: [4, 12],
         results: [],
     },
     {
         week: 7,
-        byes: [3, 12],
+        byes: [0, 12],
         results: [],
     },
     {
         week: 8,
-        byes: [4, 12],
+        // Aaron requested
+        byes: [8, 12],
         results: [],
     },
     {

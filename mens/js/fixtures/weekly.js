2430e6d7b0cf3e15196d3ba91e0a32443d1b3ebb
unknown <bradleyaw@gmail.com>
2020-07-10 14:11:19 -0400

    Update weekly rankings

--- mens/js/fixtures/weekly.js
+++ mens/js/fixtures/weekly.js
@@ -104,7 +104,22 @@ export default [
         week: 6,
         // Jake requested
         byes: [4, 12],
-        results: [],
+        results: [
+            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: -7 },
+            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: -2 },
+            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: 10 },
+            { id: 3, name: "Matt Carucci", wins: 2, losses: 1, points: 6 },
+            { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
+            { id: 5, name: "Steve Cozzolongo", wins: 1, losses: 2, points: 1 },
+            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: 2 },
+            { id: 7, name: "Josh Duc", wins: 1, losses: 2, points: -5 },
+            { id: 8, name: "Aaron Von Der Embse", wins: 3, losses: 0, points: 11 },
+            { id: 9, name: "Vasyl Martniuk", wins: 2, losses: 1, points: 6 },
+            { id: 10, name: "Adam Meier", wins: 0, losses: 3, points: -26 },
+            { id: 11, name: "Rob Opalka", wins: 0, losses: 3, points: -10 },
+            { id: 12, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
+            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: 14 }
+        ],
     },
     {
         week: 7,

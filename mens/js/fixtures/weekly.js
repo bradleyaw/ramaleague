2b8c5d1d620a32d00d54a4ab5d2978eb19de013d
unknown <bradleyaw@gmail.com>
2020-09-11 15:57:12 -0400

    weekly update

--- mens/js/fixtures/weekly.js
+++ mens/js/fixtures/weekly.js
@@ -64,15 +64,32 @@ export default [
     },
     {
         week: 4,
-        // Rob requested
-        byes: [11],
-        results: [],
+        // Rob and Jake requested
+        // 9/10
+        byes: [4, 11],
+        results: [
+            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: -9 },
+            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: 0 },
+            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: 4 },
+            { id: 3, name: "Matt Carucci", wins: 2, losses: 1, points: 6 },
+            { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
+            { id: 5, name: "Steve Cozzolongo", wins: 1, losses: 2, points: -11 },
+            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: 2 },
+            { id: 7, name: "Josh Duc", wins: 2, losses: 1, points: 4 },
+            { id: 8, name: "Aaron Von Der Embse", wins: 3, losses: 0, points: 15 },
+            { id: 9, name: "Vasyl Martniuk", wins: 2, losses: 1, points: 8 },
+            { id: 10, name: "Adam Meier", wins: 0, losses: 3, points: -8 },
+            { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
+            { id: 12, name: "Scott Poythress", wins: 0, losses: 3, points: -12 },
+            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: 5 }
+        ],
 
     },
     {
         week: 5,
-        // Josh Requested
-        byes: [7],
+        // Josh and Steve equested
+        // 9/17
+        byes: [5, 7],
         results: [],
 
     },

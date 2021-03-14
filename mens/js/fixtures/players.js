ae02f9bdd60a71f0b117c862a8200cdc96383cfc
unknown <bradleyaw@gmail.com>
2021-03-13 21:00:54 -0500

    updates"

--- mens/js/fixtures/players.js
+++ mens/js/fixtures/players.js
@@ -1,17 +1,37 @@
 export default [
-    { id: 0, name: "Garrett Bergquist", wins: 0, losses: 0, points: 0 },
-    { id: 1, name: "Lane Blundell", wins: 0, losses: 0, points: 0 },
-    { id: 2, name: "Alex Bradley", wins: 0, losses: 0, points: 0 },
-    { id: 3, name: "Matt Carucci", wins: 0, losses: 0, points: 0 },
+    { id: 0, name: "Lane Blundell", wins: 0, losses: 0, points: 0 },
+    { id: 1, name: "Alex Bradley", wins: 0, losses: 0, points: 0 },
+    { id: 2, name: "Matt Carucci", wins: 0, losses: 0, points: 0 },
+    { id: 3, name: "Peter Chavez", wins: 0, losses: 0, points: 0 },
     { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
     { id: 5, name: "Steve Cozzolongo", wins: 0, losses: 0, points: 0 },
-    { id: 6, name: "Ben Djernes", wins: 0, losses: 0, points: 0 },
-    { id: 7, name: "Josh Duc", wins: 0, losses: 0, points: 0 },
-    { id: 8, name: "Aaron Von Der Embse", wins: 0, losses: 0, points: 0 },
-    { id: 9, name: "Vasyl Martniuk", wins: 0, losses: 0, points: 0 },
-    { id: 10, name: "Adam Meier", wins: 0, losses: 0, points: 0 },
-    { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
-    { id: 12, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
-    { id: 13, name: "Peter Chavez", wins: 0, losses: 0, points: 0 }
+    { id: 6, name: "Josh Duc", wins: 0, losses: 0, points: 0 },
+    { id: 7, name: "Aaron Von Der Embse", wins: 0, losses: 0, points: 0 },
+    { id: 8, name: "Vasyl Martniuk", wins: 0, losses: 0, points: 0 },
+    { id: 9, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
+    { id: 10, name: "Evgheni Pascalov", wins: 0, losses: 0, points: 0 },
+    { id: 11, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
+    { id: 12, name: "Mauricio Reece", wins: 0, losses: 0, points: 0 },
+    { id: 13, name: "Nyle Rothschild", wins: 0, losses: 0, points: 0 },
+    { id: 14, name: "Verun Tahiliani", wins: 0, losses: 0, points: 0 },
+    { id: 15, name: "Courtney Wilson", wins: 0, losses: 0, points: 0 },
 ]
+
+//2020
+// export default [
+//     { id: 0, name: "Garrett Bergquist", wins: 0, losses: 0, points: 0 },
+//     { id: 1, name: "Lane Blundell", wins: 0, losses: 0, points: 0 },
+//     { id: 2, name: "Alex Bradley", wins: 0, losses: 0, points: 0 },
+//     { id: 3, name: "Matt Carucci", wins: 0, losses: 0, points: 0 },
+//     { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
+//     { id: 5, name: "Steve Cozzolongo", wins: 0, losses: 0, points: 0 },
+//     { id: 6, name: "Ben Djernes", wins: 0, losses: 0, points: 0 },
+//     { id: 7, name: "Josh Duc", wins: 0, losses: 0, points: 0 },
+//     { id: 8, name: "Aaron Von Der Embse", wins: 0, losses: 0, points: 0 },
+//     { id: 9, name: "Vasyl Martniuk", wins: 0, losses: 0, points: 0 },
+//     { id: 10, name: "Adam Meier", wins: 0, losses: 0, points: 0 },
+//     { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
+//     { id: 12, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
+//     { id: 13, name: "Peter Chavez", wins: 0, losses: 0, points: 0 }
+// ]
 
--- mens/js/fixtures/weekly.js
+++ mens/js/fixtures/weekly.js
@@ -1,205 +1,76 @@
 export default [
     {
+        // 3.18
+        //Jake requested
         week: 1,
-        // Garrett Scotty Requested
-        byes: [0, 12],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 0, losses: 0, points: 0 },
-            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: 3 },
-            { id: 2, name: "Alex Bradley", wins: 3, losses: 0, points: 10 },
-            { id: 3, name: "Matt Carucci", wins: 1, losses: 2, points: -2 },
-            { id: 4, name: "Jake Clopton", wins: 2, losses: 1, points: 15 },
-            { id: 5, name: "Steve Cozzolongo", wins: 2, losses: 1, points: 7 },
-            { id: 6, name: "Ben Djernes", wins: 0, losses: 3, points: -9 },
-            { id: 7, name: "Josh Duc", wins: 1, losses: 2, points: -4 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 1 },
-            { id: 9, name: "Vasyl Martniuk", wins: 2, losses: 1, points: 5 },
-            { id: 10, name: "Adam Meier", wins: 0, losses: 3, points: -19 },
-            { id: 11, name: "Rob Opalka", wins: 1, losses: 2, points: -6 },
-            { id: 12, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
-            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: -3 }
-        ]
+        byes: [1,2,3,4],
+        results: [],
     },
     {
+        // 3.25
         week: 2,
-        // Jake Requested
-        byes: [1, 4],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 2, losses: 1, points: 3 },
-            { id: 1, name: "Lane Blundell", wins: 0, losses: 0, points: 0 },
-            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: -1 },
-            { id: 3, name: "Matt Carucci", wins: 0, losses: 3, points: -13 },
-            { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
-            { id: 5, name: "Steve Cozzolongo", wins: 2, losses: 1, points: 5 },
-            { id: 6, name: "Ben Djernes", wins: 1, losses: 2, points: -10 },
-            { id: 7, name: "Josh Duc", wins: 1, losses: 2, points: -2 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 0, losses: 3, points: -11 },
-            { id: 9, name: "Vasyl Martniuk", wins: 2, losses: 1, points: 7 },
-            { id: 10, name: "Adam Meier", wins: 1, losses: 2, points: -2 },
-            { id: 11, name: "Rob Opalka", wins: 3, losses: 0, points: 14 },
-            { id: 12, name: "Scott Poythress", wins: 2, losses: 1, points: 3 },
-            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: 7 }
-        ]
+        byes: [0,5,6,7],
+        results: [],
     },
     {
+        // 4.1
+        // Aaron requested
         week: 3,
-        // Adam/Lane Requested
-        byes: [1, 10],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: 1 },
-            { id: 1, name: "Lane Blundell", wins: 0, losses: 0, points: 0 },
-            { id: 2, name: "Alex Bradley", wins: 1, losses: 2, points: -4 },
-            { id: 3, name: "Matt Carucci", wins: 2, losses: 1, points: 2 },
-            { id: 4, name: "Jake Clopton", wins: 1, losses: 2, points: 0 },
-            { id: 5, name: "Steve Cozzolongo", wins: 1, losses: 2, points: -8 },
-            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: 2 },
-            { id: 7, name: "Josh Duc", wins: 0, losses: 3, points: -12 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 8 },
-            { id: 9, name: "Vasyl Martniuk", wins: 3, losses: 0, points: 12 },
-            { id: 10, name: "Adam Meier", wins: 0, losses: 0, points: 0 },
-            { id: 11, name: "Rob Opalka", wins: 1, losses: 2, points: -7 },
-            { id: 12, name: "Scott Poythress", wins: 3, losses: 0, points: 11 },
-            { id: 13, name: "Peter Chavez", wins: 1, losses: 2, points: -5 }
-        ],
+        byes: [],
+        results: [],
     },
     {
+        // 4.8
         week: 4,
-        // Rob and Jake requested
-        // 9/10
-        byes: [4, 11],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: -9 },
-            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: 0 },
-            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: 4 },
-            { id: 3, name: "Matt Carucci", wins: 2, losses: 1, points: 6 },
-            { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
-            { id: 5, name: "Steve Cozzolongo", wins: 1, losses: 2, points: -11 },
-            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: 2 },
-            { id: 7, name: "Josh Duc", wins: 2, losses: 1, points: 4 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 3, losses: 0, points: 15 },
-            { id: 9, name: "Vasyl Martniuk", wins: 2, losses: 1, points: 8 },
-            { id: 10, name: "Adam Meier", wins: 0, losses: 3, points: -8 },
-            { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
-            { id: 12, name: "Scott Poythress", wins: 0, losses: 3, points: -12 },
-            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: 5 }
-        ],
-
+        byes: [],
+        results: [],
     },
     {
+        // 4.15
+        // Jake requested
         week: 5,
-        // Matt, Rob requested
-        // 9/24
-        byes: [3, 11],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 3, losses: 0, points: 15 },
-            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: 0 },
-            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: 4 },
-            { id: 3, name: "Matt Carucci", wins: 0, losses: 0, points: 0 },
-            { id: 4, name: "Jake Clopton", wins: 0, losses: 3, points: -8 },
-            { id: 5, name: "Steve Cozzolongo", wins: 2, losses: 1, points: 4 },
-            { id: 6, name: "Ben Djernes", wins: 1, losses: 2, points: -7 },
-            { id: 7, name: "Josh Duc", wins: 1, losses: 2, points: -7 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 6 },
-            { id: 9, name: "Vasyl Martniuk", wins: 0, losses: 3, points: -10 },
-            { id: 10, name: "Adam Meier", wins: 1, losses: 2, points: -1 },
-            { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
-            { id: 12, name: "Scott Poythress", wins: 2, losses: 1, points: 4 },
-            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: 0 }
-        ],
-
+        byes: [],
+        results: [],
     },
     {
+        // 4.22
         week: 6,
-        // Alex and Rob requested
-        // 10/1
-        byes: [2, 11],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 2, losses: 1, points: 1 },
-            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: 7 },
-            { id: 2, name: "Alex Bradley", wins: 0, losses: 0, points: 0 },
-            { id: 3, name: "Matt Carucci", wins: 0, losses: 3, points: -13 },
-            { id: 4, name: "Jake Clopton", wins: 1, losses: 2, points: 1 },
-            { id: 5, name: "Steve Cozzolongo", wins: 2, losses: 1, points: 9 },
-            { id: 6, name: "Ben Djernes", wins: 1, losses: 2, points: -5 },
-            { id: 7, name: "Josh Duc", wins: 3, losses: 0, points: 15 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 5 },
-            { id: 9, name: "Vasyl Martniuk", wins: 0, losses: 3, points: -11 },
-            { id: 10, name: "Adam Meier", wins: 1, losses: 2, points: -11 },
-            { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
-            { id: 12, name: "Scott Poythress", wins: 2, losses: 1, points: 3 },
-            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: -1 }
-        ],
+        byes: [],
+        results: [],
     },
     {
+        // 4.29
         week: 7,
-        // 10/8 
-        // Adam, Rob Requested
-        byes: [10, 11],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: -3 },
-            { id: 1, name: "Lane Blundell", wins: 2, losses: 1, points: 5 },
-            { id: 2, name: "Alex Bradley", wins: 2, losses: 1, points: -1 },
-            { id: 3, name: "Matt Carucci", wins: 2, losses: 1, points: 3 },
-            { id: 4, name: "Jake Clopton", wins: 0, losses: 3, points: -7 },
-            { id: 5, name: "Steve Cozzolongo", wins: 1, losses: 2, points: -7 },
-            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: 1 },
-            { id: 7, name: "Josh Duc", wins: 2, losses: 1, points: 3 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 5 },
-            { id: 9, name: "Vasyl Martniuk", wins: 3, losses: 0, points: 13 },
-            { id: 10, name: "Adam Meier", wins: 0, losses: 0, points: 0 },
-            { id: 11, name: "Rob Opalka", wins: 0, losses: 0, points: 0 },
-            { id: 12, name: "Scott Poythress", wins: 1, losses: 2, points: -3 },
-            { id: 13, name: "Peter Chavez", wins: 0, losses: 3, points: -9 }
-        ],
+        byes: [],
+        results: [],
     },
     {
+        // 5.6
         week: 8,
-        // 10/15
-        // Jake requested (alex also,)
-        byes: [4,3],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: -3 },
-            { id: 1, name: "Lane Blundell", wins: 1, losses: 2, points: -5 },
-            { id: 2, name: "Alex Bradley", wins: 0, losses: 0, points: 0 },
-            { id: 3, name: "Matt Carucci", wins: 1, losses: 2, points:-1},
-            { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
-            { id: 5, name: "Steve Cozzolongo", wins: 2, losses: 1, points: 6 },
-            { id: 6, name: "Ben Djernes", wins: 1, losses: 2, points: -7 },
-            { id: 7, name: "Josh Duc", wins: 3, losses: 0, points: 11 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 3, losses: 0, points: 17 },
-            { id: 9, name: "Vasyl Martniuk", wins: 1, losses: 2, points: -9 },
-            { id: 10, name: "Adam Meier", wins: 1, losses: 2, points: -3 },
-            { id: 11, name: "Rob Opalka", wins: 2, losses: 1, points: 2 },
-            { id: 12, name: "Scott Poythress", wins: 2, losses: 1, points: 12 },
-            { id: 13, name: "Peter Chavez", wins: 0, losses: 3, points: -20 }
-        ],
+        byes: [],
+        results: [],
     },
     {
+        // 5.13
         week: 9,
-        // 10/22
-        // Adam and Scotty requested
-        byes: [10 ,12],
-        results: [
-            { id: 0, name: "Garrett Bergquist", wins: 1, losses: 2, points: -5 },
-            { id: 1, name: "Lane Blundell", wins: 1, losses: 2, points: -4 },
-            { id: 2, name: "Alex Bradley", wins: 1, losses: 2, points: -2 },
-            { id: 3, name: "Matt Carucci", wins: 2, losses: 1, points: 11 },
-            { id: 4, name: "Jake Clopton", wins: 0, losses: 3, points: -15 },
-            { id: 5, name: "Steve Cozzolongo", wins: 3, losses: 0, points: 9 },
-            { id: 6, name: "Ben Djernes", wins: 2, losses: 1, points: 1 },
-            { id: 7, name: "Josh Duc", wins: 1, losses: 2, points: -5 },
-            { id: 8, name: "Aaron Von Der Embse", wins: 3, losses: 0, points: 8 },
-            { id: 9, name: "Vasyl Martniuk", wins: 1, losses: 2, points: 1 },
-            { id: 10, name: "Adam Meier", wins: 0, losses: 0, points: 0 },
-            { id: 11, name: "Rob Opalka", wins: 1, losses: 2, points: 2 },
-            { id: 12, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
-            { id: 13, name: "Peter Chavez", wins: 2, losses: 1, points: 3 }
-        ],
+        byes: [],
+        results: [],
     },
     {
+        // 5.20
         week: 10,
-        // 10/29
-        // Steve and Garrett requested
+        byes: [],
+        results: [],
+    },
+    {
+        // 5.27
+        week: 11,
+        byes: [],
+        results: [],
+    },
+    {
+        // 6.3
+        week: 12,
         byes: [],
         results: [],
     },

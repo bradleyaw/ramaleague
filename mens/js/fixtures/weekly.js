3b97a75a54a7e3a222183b789a34904ad593cd71
unknown <bradleyaw@gmail.com>
2021-04-12 17:01:01 -0400

    stadnings update

--- mens/js/fixtures/weekly.js
+++ mens/js/fixtures/weekly.js
@@ -1,15 +1,31 @@
 export default [
     {
-        // 3.18
-        //Jake requested
+        // 4.8
         week: 1,
         byes: [1,2,3,4],
-        results: [],
+        results: [
+            { id: 0, wins: 3, losses: 0, points: 20 },
+            { id: 1, wins: 0, losses: 0, points: 0 },
+            { id: 2, wins: 1, losses: 2, points: -14 },
+            { id: 3, wins: 1, losses: 2, points: -4 },
+            { id: 4, wins: 2, losses: 1, points: -1 },
+            { id: 5, wins: 1, losses: 2, points: -2 },
+            { id: 6, wins: 1, losses: 2, points: 2 },
+            { id: 7, wins: 0, losses: 0, points: 0 },
+            { id: 8, wins: 1, losses: 2, points: -6 },
+            { id: 9, wins: 0, losses: 0, points: 0 },
+            { id: 10, wins: 3, losses: 0, points: 12 },
+            { id: 11, wins: 0, losses: 0, points: 0 },
+            { id: 12, wins: 2, losses: 1, points: 7 },
+            { id: 13, wins: 2, losses: 1, points: 5 },
+            { id: 14, wins: 0, losses: 3, points: -11 },
+            { id: 15, wins: 1, losses: 2, points: 8 },
+        ],
     },
     {
-        // 3.25
+        // 4.15
         week: 2,
-        byes: [0,5,6,7],
+        byes: [4,5,11,15],
         results: [],
     },
     {
--- mens/js/mensService.js
+++ mens/js/mensService.js
@@ -1,7 +1,15 @@
 import players from './fixtures/players.js';
 
+const cmp = (a, b) => (a > b) - (a < b);
+
 export const rankPlayers = (playerArray) => {
-    return playerArray.sort(function (a, b) { return b.wins/b.losses - a.wins/a.losses || b.points - a.points })
+    return playerArray.sort(function (a, b) {
+        const aWinLoss = Number.isNaN(a.wins/a.losses) ? 1 : a.wins/a.losses;
+        const bWinLoss = Number.isNaN(b.wins/b.losses) ? 1 : b.wins/b.losses;
+        console.log(aWinLoss)
+        console.log(bWinLoss)
+        return cmp(bWinLoss, aWinLoss) || cmp(b.points,a.points) 
+    })
 }
 
 export const updateStandings = (resultsArray) => {

a87401e26ca010bc67968bd7082cb2ac2d5019a7
unknown <bradleyaw@gmail.com>
2021-04-20 17:31:41 -0400

    weekly update

--- mens/js/fixtures/weekly.js
+++ mens/js/fixtures/weekly.js
@@ -26,66 +26,83 @@ export default [
         // 4.15
         week: 2,
         byes: [4,5,11,15],
-        results: [],
+        results: [
+            { id: 0, name: "Lane Blundell", wins: 2, losses: 1, points: 9 },
+            { id: 1, name: "Alex Bradley", wins: 0, losses: 3, points: -16 },
+            { id: 2, name: "Matt Carucci", wins: 0, losses: 3, points: -11 },
+            { id: 3, name: "Peter Chavez", wins: 2, losses: 1, points: 5 },
+            { id: 4, name: "Jake Clopton", wins: 0, losses: 0, points: 0 },
+            { id: 5, name: "Steve Cozzolongo", wins: 0, losses: 0, points: 0 },
+            { id: 6, name: "Josh Duc", wins: 2, losses: 1, points: 4 },
+            { id: 7, name: "Aaron Von Der Embse", wins: 2, losses: 1, points: 0 },
+            { id: 8, name: "Vasyl Martniuk", wins: 2, losses: 1, points: -1 },
+            { id: 9, name: "Rob Opalka", wins: 2, losses: 1, points: 12 },
+            { id: 10, name: "Evgheni Pascalov", wins: 2, losses: 1, points: 11 },
+            { id: 11, name: "Scott Poythress", wins: 0, losses: 0, points: 0 },
+            { id: 12, name: "Mauricio Reece", wins: 2, losses: 1, points: -3 },
+            { id: 13, name: "Nyle Rothschild", wins: 0, losses: 3, points: -17 },
+            { id: 14, name: "Verun Tahiliani", wins: 2, losses: 1, points: 7 },
+            { id: 15, name: "Courtney Wilson", wins: 0, losses: 0, points: 0 },
+        ],
     },
     {
-        // 4.1
-        // Aaron requested
+        // Scotty Alex requested
+        // 4.22
         week: 3,
         byes: [],
         results: [],
     },
     {
-        // 4.8
+        // Scotty Alex requested
+        // 4.29
         week: 4,
         byes: [],
         results: [],
     },
     {
-        // 4.15
-        // Jake requested
+        // 5.6
         week: 5,
         byes: [],
         results: [],
     },
     {
-        // 4.22
+        // 5.13
         week: 6,
         byes: [],
         results: [],
     },
     {
-        // 4.29
+        // 5.20
         week: 7,
         byes: [],
         results: [],
     },
     {
-        // 5.6
+        // 5.27
         week: 8,
         byes: [],
         results: [],
     },
     {
-        // 5.13
+        // 6.3
         week: 9,
         byes: [],
         results: [],
     },
     {
-        // 5.20
+        // 6.10
         week: 10,
         byes: [],
         results: [],
     },
     {
-        // 5.27
+        // 6.17
         week: 11,
         byes: [],
         results: [],
     },
     {
-        // 6.3
+        // 6.24
         week: 12,
         byes: [],
         results: [],
